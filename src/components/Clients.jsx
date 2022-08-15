7f80170fcc470378cb2b5391053b260ab899d8c1
it devpermata <itdevpermata@its-MacBook-Air-2.local>
2022-08-15 09:22:15 +0700

    inital commit

---
+++ src/components/Clients.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { clients } from "../constants";
+import styles from "../styles";
+
+const Clients = () => (
+<section className={`${styles.flexCenter} my-4`}>
+  <div className={`${styles.flexCenter} flex-wrap w-full `}>
+    {clients.map((client) => (
+      <div className={`${styles.flexCenter} flex-1 sm:min-w-[192px] min-w-[120px] `} key={client.id}>
+        <img src={client.logo} alt="client" className="sm:w-[192px] w-[100px] object-contain hover:bg-dimWhite"/>
+      </div>
+    ))}
+  </div>
+</section>);
+;
+
+export default Clients;
